666b5436544b8b67b1b23fa8fd95ed42eeea0b83
Philippe Caron <amazingphilippe@gmail.com>
2022-01-13 23:35:28 -0500

    80080

---
+++ src/generative/80080/index.md
@@ -0,0 +1,7 @@
+---
+title: "800x80"
+width: 800
+height: 80
+---
+
+Draw 80 triangles. All triangle must have two points on the same vertical axis, touching both the top and bottom of the canvas. The third point must point towards the right and be chosen at random. All triangle have the same width. 
---
+++ src/generative/80080/script.js
@@ -0,0 +1,38 @@
+import { SVG } from "https://cdn.skypack.dev/@svgdotjs/svg.js";
+
+import {
+  random,
+  map,
+} from "https://cdn.skypack.dev/@georgedoescode/generative-utils@1.0.34";
+
+import chroma from "https://cdn.skypack.dev/chroma-js";
+
+const svg = SVG(".canvas");
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+  generate();
+});
+
+document.addEventListener("keyup", (e) => {
+  if (e.code === "Space") {
+    generate();
+  }
+});
+
+const { width, height } = svg.viewbox();
+const palette = ["#362310"];
+
+
+//-----------------------------------------
+function generate() {
+  svg.clear();
+  let color = "#B7CE63";
+
+  for (var i = 0; i < 80; i++) {
+    svg.polyline([[i*10, 0], [i*10, height], [(i * 10 ) + 10, random(10, height - 10, true)]]).fill(color);
+    color = chroma(color).set('lch.h', `*${random(-2, 2)}`).hex();
+    console.log(color);
+  }
+}
+generate();
