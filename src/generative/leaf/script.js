49e84d2187458502d0da12553d0aa60ea8b8ead0
Philippe Caron <amazingphilippe@gmail.com>
2022-03-06 16:39:20 -0500

    leaf

--- src/generative/leaf/script.js
+++ src/generative/leaf/script.js
@@ -31,37 +31,42 @@ paper.setup(document.getElementById("shadow"));
 //-----------------------------------------
 
 function generate() {
-  svg.clear();
+  //svg.clear();
+  //svg.rect(width, height).attr({x: 0, y: 0}).fill("#DEDEE0").css({opacity: 0.25})
   let size = random(5, 10);
   let points = [];
+  let type = random(0, 1);
 
   for (var a = 1; a < Math.PI * 2; a += Math.PI / 60) {
     let r = size;
-    let sec_a = (1/Math.cos(a));
+    let sec_a = 1 / Math.cos(a);
     let folium;
     //folium = (3 * a * Math.sin(a) * Math.cos(a)) / (Math.pow(Math.sin(a), 3) + Math.pow(Math.cos(a), 3));
     //folium = 6 * a * Math.cos(a / 3);
     //folium = (5 / 2) * (4 * Math.cos(a) - (1/Math.cos(a)));
     //folium = r * (Math.sin(3*a) / Math.sin(2*a));
-    folium = (r / 2) * ((4 * Math.cos(a)) - (1/Math.cos(a))); //Trisectrix of Maclaurin
-    //folium = (3 * r * sec_a * Math.tan(a)) / (1 + Math.pow(Math.tan(a), 3)) //Folium
+    if (type > 0.5) {
+      folium = (r / 2) * (4 * Math.cos(a) - 1 / Math.cos(a)); //Trisectrix of Maclaurin
+    } else {
+      folium = (3 * r * sec_a * Math.tan(a)) / (1 + Math.pow(Math.tan(a), 3)); //Folium
+    }
     let x = r * folium * Math.cos(a);
     let y = r * folium * Math.sin(a);
 
-    if (x > 0 ) {
-      points.push([
-        x + width / 2,
-        y + width / 2
-      ]);
+    if (x > 0 && y > 0) {
+      points.push([x + width / 2, y + width / 2]);
     }
-
   }
 
   //points.push(points[0]);
   console.log(points);
-
+console.log(chroma("#311E10").saturate(random(-2, 2)));
   //debug(points)
-  svg.polyline(points).stroke({ width: 1, color: "black" }).fill("none");
+  svg
+    .polyline(points)
+    .stroke({ width: 1, color: "#DEDEE0" })
+    .fill(chroma("#311E10").saturate(random(-1, 1)).hex())
+    .transform({ rotate: random(0, 360), origin: [width / 2, height / 2] });
   //svg.circle(3).attr({cx: width / 2, cy: height / 2}).fill("red");
 
   // let halfA = new paper.Path.Circle(new paper.Point(width/2 - size, height/2), (height/2) * 0.667);
@@ -79,6 +84,9 @@ generate();
 
 function debug(points) {
   for (var i = 0; i < points.length; i++) {
-    svg.circle(2).attr({cx: points[i][0], cy: points[i][1]}).fill(chroma(125, i/points.length, 0.5, 'hsl').hex());
+    svg
+      .circle(2)
+      .attr({ cx: points[i][0], cy: points[i][1] })
+      .fill(chroma(125, i / points.length, 0.5, "hsl").hex());
   }
 }
