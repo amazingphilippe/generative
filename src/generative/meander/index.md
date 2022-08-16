5d6bee98b635a2638130025d70b9a42a619cf463
Philippe Caron <amazingphilippe@gmail.com>
2022-08-16 00:47:31 -0400

    meander

---
+++ src/generative/meander/index.md
@@ -0,0 +1,9 @@
+---
+title: "Meander"
+width: 400
+height: 400
+---
+
+Draw between 200 and 800 points. Starting with any point, find the nearest points. Draw a line to the lowest adjacent point. Repeat until it is impossible to continue, then draw a dot at the end of the line. Repeat using any remaining point, until there is no point left.
+
+This piece uses the methodology explained in [Algorithmic landscapes : computational methods for the mediation of form, information, and performance in landscape architecture](https://www.repo.uni-hannover.de/handle/123456789/3768)
---
+++ src/generative/meander/script.js
@@ -0,0 +1,179 @@
+import { SVG } from "https://cdn.skypack.dev/@svgdotjs/svg.js";
+// plugins for svg dot js
+import "https://cdn.skypack.dev/@svgdotjs/svg.topath.js";
+
+import {
+  createVoronoiTessellation,
+  createNoiseGrid,
+  random,
+  map,
+} from "https://cdn.skypack.dev/@georgedoescode/generative-utils@1.0.34";
+
+import {
+  parsePath,
+  roundCorners,
+} from "https://cdn.skypack.dev/svg-round-corners";
+
+import paper from "https://cdn.skypack.dev/paper";
+
+import chroma from "https://cdn.skypack.dev/chroma-js";
+
+const svg = SVG(".canvas");
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+  generate();
+});
+
+document.addEventListener("keyup", (e) => {
+  if (e.code === "Space") {
+    generate();
+  }
+});
+
+paper.setup(document.getElementById("shadow"));
+
+// const palette = ["#f2af29", "#533e2d", "#058c42", "#9ee493"];
+const palette = ["#f1f7ed","#243e36","#7ca982","#e0eec6","#c2a83e"];
+// const palette = ["#8C1C13", "#FFD400", "#34623F", "#2C497F"]
+const { width, height } = svg.viewbox();
+
+//-----------------------------------------
+
+function generate() {
+  svg.clear();
+  let density = random(200, 800, true);
+
+  let grid = createNoiseGrid({
+    width,
+    height,
+    resolution: 48,
+    xInc: 0.0125,
+    yInc: 0.0125,
+    seed: Math.random() * 1000,
+  });
+
+  let tessellation = createVoronoiTessellation({
+    width: width,
+    height: height,
+    points: [...Array(density)].map(() => {
+      return {
+        x: random(0, width, true),
+        y: random(0, height, true),
+      };
+    }),
+    relaxIterations: 2,
+  });
+
+  // Draw each point once
+  // tessellation.cells.forEach((c) => {
+  //   svg
+  //     .circle(2)
+  //     .x(c.centroid.x - 1)
+  //     .y(c.centroid.y - 1)
+  //     .fill(random(palette));
+  // });
+
+  // Find the average distance to the closest point for every point.
+  let averageDistance = 0;
+  tessellation.cells.forEach((c1, i) => {
+    let closest = 9999;
+    tessellation.cells.forEach((c2, j) => {
+      if (i != j) {
+        let d = distance(c1, c2);
+        if (d < closest) {
+          closest = d;
+        }
+      }
+    });
+
+    averageDistance += closest;
+  });
+
+  averageDistance = (averageDistance / density) * 1.6;
+
+  tessellation.cells.sort((a, b) => a.centroid.x - b.centroid.x);
+  tessellation.cells.sort((a, b) => a.centroid.y - b.centroid.y);
+
+  let stuck = false;
+  let count = 0;
+  let root = tessellation.cells.shift();
+  let polyline = [root];
+
+  //Layers
+  let lines = svg.group();
+  let ends = svg.group();
+
+  while (!stuck) {
+    let candidates = [];
+    tessellation.cells.forEach((c, i) => {
+      let d = distance(c, root);
+      if (d < averageDistance) {
+        candidates.push(c);
+      }
+    });
+
+    if (candidates.length == 0 || count > 1000) {
+      if (tessellation.cells.length > 2) {
+        let endRadius = random(10, 20);
+        ends
+          .circle(endRadius)
+          .x(root.centroid.x - endRadius / 2)
+          .y(root.centroid.y - endRadius / 2)
+          .fill(random(palette));
+
+        let polylinePath = new paper.Path(
+          polyline.flatMap((c) => [[c.centroid.x, c.centroid.y]])
+        );
+
+        let color = chroma(random(palette)).brighten(random(-2, 1)).hex();
+        lines
+          // .path(polylinePath.pathData)
+          .path(roundCorners(polylinePath.pathData, 30).path)
+          .fill("none")
+          .stroke({
+            color: color,
+            width: chroma(color).luminance() + 1,
+            linecap: "round",
+            linejoin: "round"
+          });
+
+        // Find new root
+        root = tessellation.cells.shift();
+        polyline = [root];
+        continue;
+      }
+
+      stuck = true;
+      console.log(`Stuck! After ${count} iterations`);
+    }
+
+    candidates.sort((a, b) => a.centroid.y - b.centroid.y);
+
+    // Shift the lowest Y from candidates
+    let add = candidates.shift();
+
+    // Remove chosen candidates from remaining points
+    let remove = tessellation.cells.findIndex((item) => item == add);
+    tessellation.cells.splice(remove, 1);
+
+    if (add) {
+      // lines.line(root.centroid.x, root.centroid.y, add.centroid.x, add.centroid.y, ).fill("none").stroke({ color: "#423", width: 3, linecap: "round" });
+      polyline.push(add);
+    } else {
+    }
+
+    root = add;
+    //console.log(root);
+
+    count++;
+  }
+}
+generate();
+
+function distance(a, b) {
+  return Math.sqrt(
+    (b.centroid.x - a.centroid.x) * (b.centroid.x - a.centroid.x) +
+      (b.centroid.y - a.centroid.y) * (b.centroid.y - a.centroid.y)
+  );
+}
